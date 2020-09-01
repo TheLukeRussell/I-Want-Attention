42d47293f69e57980d40d2f2d1ebcd7714a6d320
Luke Russell <thelukerussel21@gmail.com>
2020-09-01 16:39:15 -0400

    wireframe finished

--- App.js
+++ App.js
@@ -2,13 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
+import { preventAutoHide } from 'expo/build/launch/SplashScreen';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
+  const [outputText, setOutputText] = useState('Click the button if you want attention');
   return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title='Change Text' onPress={() => setOutputText('The text has been changed')} />
+      <Text style={styles.text}>{outputText}</Text>
+      <Button
+        buttonStyle={{
+          backgroundColor: '#3f4441',
+          width: '100%',
+          height: '50%',
+        }}
+        title='I want Attention'
+        onPress={() => setOutputText('Luke has been notified!')}
+      />
+      {/* <Button
+        buttonStyle={{ backgroundColor: '#5e6f64', width: '100%' }}
+        title='Change Text'
+        onPress={() => setOutputText('The text has been changed')}
+      /> */}
       <StatusBar style='auto' />
     </View>
   );
@@ -17,8 +31,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5e6f64',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    paddingVertical: 10,
   },
 });
